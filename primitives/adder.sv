81f47aa0d556b63084d1ce791ee799baa820596e
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2024-07-25 10:05:04 +0545

    Build 8bit adder

--- primitives/adder.sv
+++ primitives/adder.sv
@@ -1,19 +1,22 @@
 `timescale 1ns/1ps;
 
-module half_adder(a, b, c_out, s);
-   input a, b;
-   output c_out, s;
+module half_adder(
+                  input a, b,
+                  output c_out, s
+                  );
 
    assign c_out = a & b;
    assign s = a ^ b;
 endmodule; // half_adder
 
 
-module full_adder(a, b, c_in, c_out, s);
-   input a, b, c_in;
-   output c_out, s;
-   wire   i1, i2, i3;
+module full_adder(
+                  input a, b, c_in,
+                  output s, c_out
+                  );
 
+
+   wire                  i1, i2, i3;
    half_adder ha1 (
                    .a(a),
                    .b(b),
@@ -32,4 +35,27 @@ module full_adder(a, b, c_in, c_out, s);
 
 endmodule; // full_adder
 
+module adder8(
+              input [7:0] A,
+              input [7:0] B,
+              input c_in,
+              output [7:0] S,
+              output c_out
+              );
 
+
+
+   wire [7:0]        carry;
+
+   full_adder fa1 (A[0], B[0], c_in, S[0], carry[0]);
+   full_adder fa2 (A[1], B[1], carry[0], S[1], carry[1]);
+   full_adder fa3 (A[2], B[2], carry[1], S[2], carry[2]);
+   full_adder fa4 (A[3], B[3], carry[2], S[3], carry[3]);
+   full_adder fa5 (A[4], B[4], carry[3], S[4], carry[4]);
+   full_adder fa6 (A[5], B[5], carry[4], S[5], carry[5]);
+   full_adder fa7 (A[6], B[6], carry[5], S[6], carry[6]);
+   full_adder fa8 (A[7], B[7], carry[6], S[7], carry[7]);
+
+   assign c_out = carry[7];
+
+endmodule; // adder8
--- primitives/test_adder.py
+++ primitives/test_adder.py
@@ -5,18 +5,53 @@ from cocotb.triggers import Timer
 
 from itertools import product
 
+def delay_ns(ns: int):
+    '''
+    pyright and mypy don't like it when you assign a number to
+    something that has one of the numbers.<types>.
+    '''
+    return Timer(ns, units='ns') # type: ignore
+
+
+def dec_to_binarray(dec_in: int):
+    ret = []
+    while dec_in != 0:
+        ret.append(dec_in % 2)
+        dec_in //= 2
+    return ret
+
+# @cocotb.test
+# async def test_full_adder(dut):
+#
+#     for a, b, c_in in product([0,1],[0,1],[0,1]):
+#         dut.a.value = a
+#         dut.b.value = b
+#         dut.c_in.value = c_in
+#
+#         s = a+b+c_in
+#         c_out = s // 2
+#         s = s % 2
+#         await delay_ns(1)
+#         assert dut.s.value == s, f"Expected {s}, got {dut.s.value}"
+#         assert dut.c_out.value == c_out, f"Expected {c_out}, got {dut.c_out.value}"
+
+
+
 @cocotb.test
-async def test_full_adder(dut):
+async def test_adder8(dut):
 
-    for a, b, c_in in product([0,1],[0,1],[0,1]):
-        dut.a.value = a
-        dut.b.value = b
-        dut.c_in.value = c_in
+    for a, b in zip(range(2**8), range(2**8)):
+        for c_in in [0,1]:
+            dut.A.value = a
+            dut.B.value = b
+            dut.c_in.value = c_in
 
-        s = a+b+c_in
-        c_out = s // 2
-        s = s % 2
-        await Timer(1, units="ns")
-        assert dut.s.value == s, f"Expected {s}, got {dut.s.value}"
-        assert dut.c_out.value == c_out, f"Expected {c_out}, got {dut.c_out.value}"
+            # ground truths
+            s = a+b+c_in
+            c_out = s // (2**8)
+            s = s % (2**8)
 
+            await delay_ns(1)
+
+            assert dut.S.value == s, f"Expected {s}, got {dut.S.value}"
+            assert dut.c_out.value == c_out, f"Expected {c_out}, got {dut.c_out.value}"
